ba67cf7a7f0eb20301f6764fa9c0c4a4f8f0d1b6
Jake O'Toole <otoolej33@gmail.com>
2020-06-26 10:41:39 -0500

    Connected to database and ran a test query.

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+// database
+// verbose turns on messages in the terminal regarding the state of the runtime
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to the database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+})
+
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not FOund) Catch all
 // -- needs to come after the routes
@@ -14,7 +30,10 @@ app.use((req,res)=>{
 })
 
 // Start server
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}!`);
-});
+// -- wrapped function ensures that server starts after conenction to database
+db.on('open', ()=> {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}!`);
+    });
+})
 
