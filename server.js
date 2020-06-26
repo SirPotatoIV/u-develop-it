b10d8720bfd8d945dd39bbcd5fb59eaa1772c714
Jake O'Toole <otoolej33@gmail.com>
2020-06-26 11:04:25 -0500

    created database-queries

--- server.js
+++ server.js
@@ -19,8 +19,45 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 })
 
-db.all(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// Gets all the rows from the database.
+// -- response is called rows because each row is returned as an object
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id=15`, (err, row)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+// DELETE a candidate
+// Run executes the SQL query, but does not retrieve any result data
+// Question mark denotes a placeholder, making this a prepared statement
+// The next argument is a param, which is currently hardcoded as 1
+// -- param can be an array if we need multiple values
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result){
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// })
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES(?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+//ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
 });
 
 // Default response for any other request (Not FOund) Catch all
